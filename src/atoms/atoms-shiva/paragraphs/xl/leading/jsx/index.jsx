d037811f4fdfd2d7e421b3ed1e3fdeda18889aa1
Shiva pandey <shivapandey9898@gmail.com>
2024-07-30 15:00:26 +0530

    feat: created atom for paragraph xl light jsx

---
+++ src/atoms/atoms-shiva/paragraphs/xl/leading/jsx/index.jsx
@@ -0,0 +1,12 @@
+const ParagraphXlLightJsx = ( props ) => {
+
+    return (
+
+        <p class="text-xl leading-8 font-roboto">
+            { props.children }
+        </p>
+
+    )
+
+}
+export default ParagraphXlLightJsx
