02a6b008c23e7ff0bc38cf66e22ea5a7ca6ffea5
Shiva pandey <shivapandey9898@gmail.com>
2024-08-02 09:52:21 +0530

    refactor: changed color name for button purple

--- src/atoms/atoms-shiva/buttons/purple/jsx/index.jsx
+++ src/atoms/atoms-shiva/buttons/purple/jsx/index.jsx
@@ -2,7 +2,7 @@ const ButtonPurpleJsx = ( props ) => {
 
     return (
 
-        <button className="bg-purple-500 rounded-full px-6 py-3 text-white font-semibold">
+        <button className="bg-purple rounded-full px-6 py-3 text-white font-semibold">
             { props.children }
         </button>
 
